de5e41e649d3a86bde424c24cdf5bf2b157293bc
Rajesh20243034 <rajesh.patelp3034@gmail.com>
2015-03-02 19:07:03 +0530

    alpha version compeleted

--- index.php
+++ index.php
@@ -6,6 +6,32 @@
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-	
+	<div class="indianflagdiv" id="indianflagdiv">
+		<div class="indflaginner" id="indflaginner">
+			<div class="indflagmainpart" id="indflagmainpart">
+				<div class="reddivtop" id="reddivtop">
+				</div>
+				<div class="whitedivmiddle" id="whitedivmiddle">
+					<img class="img-responsive" src="ashoka_chakra.png"/>
+				</div>
+				<div class="greendivbottom" id="greendivbottom">
+				</div>
+			</div>
+			<div class="rodflag" id="rodflag">
+			</div>
+			<div class="fourthround" id="fourthround">
+			
+			</div>
+			<div class="iiirdround" id="iiirdround">
+			
+			</div>
+			<div class="iindround" id="iindround">
+			
+			</div>
+			<div class="istroung" id="istRound">
+
+			</div>
+		</div>
+	</div>
 </body>
 </html>
